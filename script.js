34479ad758b22beda637140e3061e61dc38ab883
Luis Miguel_AuraGod69 <luis.bernardo.silva@escola.pr.gov.br>
2026-06-08 16:37:14 -0300

    retirada calc

--- script.js
+++ script.js
@@ -50,30 +50,6 @@ toggleButton.addEventListener("click", () => {
             "🌙 Tema Escuro";
     }
 
-    // CALCULADORA DE SEMENTES
-
-const botaoCalc = document.getElementById("calcular-sementes");
-
-botaoCalc.addEventListener("click", () => {
-
-    const area = Number(document.getElementById("area").value);
-    const linha = Number(document.getElementById("linha").value);
-    const planta = Number(document.getElementById("planta").value);
-    const germ = Number(document.getElementById("germinacao").value) / 100;
-
-    const resultado = document.getElementById("resultado-sementes");
-
-    if (!area || !linha || !planta || !germ) {
-        resultado.textContent = "Preencha todos os campos corretamente.";
-        return;
-    }
-
-    const areaPorPlanta = linha * planta;
-    const plantas = area / areaPorPlanta;
-    const sementes = plantas / germ;
-
-    resultado.textContent =
-        `Quantidade aproximada: ${Math.ceil(sementes)} sementes`;
-    });
 });
+
 
